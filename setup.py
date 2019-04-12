d8482cd00e33ce587f62bd694527a2650261d894
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-04-12 14:03:40 +1000

    Fixes

--- setup.py
+++ setup.py
@@ -3,23 +3,20 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r") as fh:
-    install_requires = fh.readlines()
-
 setup(
     name="pacemaker-mkeshav",
     version="0.1.0",
     author="Keshav Murthy",
     author_email="mkeshav@gmail.com",
-    description="A small genie to generate test data",
+    description="To keep the old heart ticking",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/mkeshav/pace-maker.git",
-    packages=find_packages(include=('genie_pkg',)),
+    packages=find_packages(include=('pacemaker',)),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=install_requires
+    install_requires=[]
 )
