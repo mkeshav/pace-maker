43d3d46e8b0d13ecc7b7f2ecd2239a68a6124e92
Keshav Murthy <keshav.murthy@latitudefinancial.com>
2019-04-13 13:27:16 +1000

    Increment version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pacemaker-mkeshav",
-    version="0.1.0",
+    version="0.2.0",
     author="Keshav Murthy",
     author_email="mkeshav@gmail.com",
     description="To keep the old heart ticking",
